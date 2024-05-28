c8f2b72025695e0214d69bc068676d2116cce042
Augustine Nani <augustine.nani@hubtel.com>
2024-05-28 14:26:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello from JavaScript!");
+
+import { createRoot } from "react-dom/client";
+
+function NavigationBar() {
+  // TODO: Actually implement a navigation bar
+  return <h1>Hello from React!</h1>;
+}
+
+
+const domNode = document.getElementById("navigation");
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
